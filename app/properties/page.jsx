34a28dd2b2558b21c968d7552acbef948e628ede
Quyen Hoang <quyen.h@dssolution.jp>
2024-06-04 04:09:50 +0700

    file-base routing, server vs client componet

---
+++ app/properties/page.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Link from 'next/link'
+
+const Properties = () => {
+  return (
+    <div>
+      <h1 className="text-3xl">Properties Page</h1>
+      <Link href="/">Go Home</Link>
+    </div>
+  )
+}
+
+export default Properties
